805b84e5c65751b89c11f362e7c35d11043510e0
Deer404 <919187569@qq.com>
2024-07-26 00:34:10 +0800

    chore: Update tsconfig.json and rollup.config.js for improved build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,27 @@
-import typescript from "@rollup/plugin-typescript";
 import preserveDirectives from "rollup-preserve-directives";
-
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.js",
-      format: "esm",
-    },
-  ],
-  plugins: [preserveDirectives(), typescript({ tsconfig: "tsconfig.json" })],
-  external: ["react", "react-dom"],
-};
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "esm",
+        preserveDirectives: true,
+      },
+    ],
+    plugins: [
+      preserveDirectives(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+      }),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
