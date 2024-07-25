4b03d8b1cabdffaae66f1a6fb6e2e26c932e4737
Deer404 <919187569@qq.com>
2024-07-25 23:13:36 +0800

    chore: Update package.json with repository URL

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,15 @@
 import typescript from "@rollup/plugin-typescript";
 import preserveDirectives from "rollup-plugin-preserve-directives";
-const config = [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        dir: "dist",
-        format: "esm",
-        preserveModules: true,
-      },
-    ],
-    plugins: [preserveDirectives(), typescript()],
-    external: ["react", "react-dom"],
-  },
-];
 
-export default config;
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      dir: "dist",
+      format: "esm",
+      preserveModules: true,
+    },
+  ],
+  plugins: [preserveDirectives(), typescript()],
+  external: ["react", "react-dom"],
+};
